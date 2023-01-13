f4516c5eb7d4b1756258a47c109d42810d36c4f3
r-l-tharp <r.l.tharp@outlook.com>
2023-01-13 14:45:44 -0600

    Partial update of CV.js with formatting  errors at Professional Experience

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -7,13 +7,8 @@ const Cv = () => {
 <tr>
 <td colspan="2" valign="top"><p>RACHEL THARP</p>
 
-<p><a href="mailto:rlt@ou.edu">rlt@ou.edu</a> Variorum Chaucer</p>
-
-<p>(417)459.0590 21 Bizzell Library</p>
-
-<p>2023 Delaware Ln 401 W Brooks St</p>
-
-<p>Norman, OK 73071 Norman, OK 73019</p>
+<p><a href="mailto:r.l.tharp@outlook.com">r.l.tharp@outlook.com</a></p>
+<p><a href="www.linkedin.com/in/rachel-tharp/">Linkedin</a></p>
 
 </td>
 </tr>
@@ -24,11 +19,11 @@ const Cv = () => {
 
 <p>English Department, University of Oklahoma, Norman, OK</p>
 
-<p>Dissertation: “Accent and Embellishment / A History of the Virgule from Grammar Handbook to Liturgical Leaf”</p>
+<p>Dissertation: <em>“Virgulae</em> and <em>Distinctiones</em>: A History of Western Notational Systems from Grammar Handbook to Liturgical Leaf”</p>
 
-<p>Dissertation Committee: Profs. Daniel Ransom, Samuel Huskey, Joyce Coleman, William Endres, &amp; David Anderson</p>
+<p>Dissertation Committee: Profs. Daniel Ransom, Samuel Huskey, Joyce Coleman, William Endres, &amp; Joseph Mansky</p>
 
-<p>I present the history of the virgule or slash (/)as a measure of how students read and interpreted texts first by methods taught in grammar handbooks but then according to scribal-driven innovations to the page. The mark primarily operates in four ways: as an outsider to the dominant mode of punctuating from the first century CE to the ninth; as the term <em>virgula, </em>a “stem” or “twig” describing the shapes of accents or critical signs in grammatical treatises; as a run-over mark and byproduct of the Irish preference for content over calligraphy when copying manuscripts for export in the eighth and ninth centuries; and as a symbol perhaps descended from the acute accent and the neume <em>virga </em>in plainchant musical notation of the ninth to eleventh centuries. The history of the virgule reveals how the responsibilities once considered as acts of interpretation left to the reader became expectations for the appearance of texts placed upon the scribe.</p>
+<p>I present the history of the virgule or slash (/) as a measure of how students read and interpreted texts first by methods taught in grammar handbooks but then according to scribal-driven innovations to the page. The mark primarily operates in four ways: as an outsider to <em>distinctiones</em>, the dominant mode of punctuating from the first century CE to the ninth; as the term <em>virgula</em>, a “stem” or “twig” describing the shapes of accents or critical signs in grammatical treatises; as a runover or punctuation mark, both byproducts of Insular innovations in book production during the eighth and ninth centuries; and as an outsider yet again in Carolingian scriptoria, where scribes preferred a modified version of <em>distinctiones</em> or a new system of liturgical punctuation called <em>positurae</em>. The history of the virgule reveals how the responsibilities once considered as acts of interpretation left to the reader became expectations for the appearance of texts placed upon the scribe.</p>
 
 <p><strong>MA, 2017 </strong></p>
 
@@ -49,20 +44,27 @@ const Cv = () => {
 
 <tr>
 <td valign="top"><p>PUBLICATIONS</p></td>
-<td valign="top"><p><strong>Manuscripts Under Review </strong></p>
+<td valign="top"><p><strong>Accepted</strong></p>
 
-<p>“‘Under this Yok of Mariage Ybounde:’ Aristocratic Husbands and Authoritative Wives in the Merchant’s Tale.” <em>Journal of the Midwest Modern Language Association.</em></p>
+<p>“‘Under this Yok of Mariage Ybounde:’ Aristocratic Husbands and Authoritative Wives in the Merchant’s Tale.” <em>Journal of the Midwest Modern Language Association</em>, Special Issue on Confinement, expected 2023.</p>
 
-<p><strong>Manuscripts in Preparation</strong></p>
+<p><strong>In Preparation</strong></p>
 
-<p>“Reading Unseparated Text: <em>Distinctiones</em> in First- to Seventh-Century Grammars &amp; Carolingian-Period Codices.”</p>
+<p>“Innovation and Exemplars: The Virgule’s Role in Insular Book Production from the Seventh to Ninth Centuries.” <em>Viator</em>.</p>
 
+<p>“<em>Virgulae</em>, <em>Distinctiones</em>, and <em>Positurae</em>: Notational Competition in Eighth- and Ninth-Century Carolingian Manuscripts.” <em>Medium Aevum</em>.</p>
 </td>
 </tr>
-
+<tr>
+    <td valign="top">GRANTS</td>
+    <td valign="top"><p> 2022, Phi Kappa Phi, Graduate Research Grant, $1,500</p>
+    <p> 2022, University of Oklahoma Graduate Student Senate, Research Grant, $750</p>
+    </td>
+</tr>
 <tr>
 <td valign="top"><p>AWARDS &amp; FELLOWSHIPS</p></td>
-<td valign="top"><p>2021-2022, English Department Dissertation Fellowship, University of Oklahoma</p>
+<td valign="top"><p>2022, Donald R. Howard Travel Fund Scholarship, New Chaucer Society</p>
+    <p>2021-2022, English Department Dissertation Fellowship, University of Oklahoma</p>
 
 <p>2021,Derdeyn-CMRS Travel Scholarship, University of Oklahoma </p>
 
@@ -87,23 +89,23 @@ const Cv = () => {
 <td valign="top"><p>CONFERENCE ACTIVITY</p></td>
 <td valign="top"><p><strong>Papers Presented </strong></p>
 
-<p>2022, “Digitizing the John Manly and Edith Rickert Canterbury Tales Collation Cards,” New Chaucer Society Congress, July 11-14.</p>
+<p>2022, “Digitizing the John Manly and Edith Rickert Canterbury Tales Collation Cards,” New Chaucer Society Congress, July 11-14</p>
 
-<p>2022, “The Virgule’s Role in Insular Book Production During the Eighth and Ninth Centuries,” International Congress on Medieval Studies, May 9-14.</p>
+<p>2022, “The Virgule’s Role in Insular Book Production During the Eighth and Ninth Centuries,” International Congress on Medieval Studies, May 9-14</p>
 
-<p>2021, “The Ancient Origins of the Virgule: the Space between Words and Symbols,” International Medieval Congress, July 5-9.</p>
+<p>2021, “The Ancient Origins of the Virgule: the Space between Words and Symbols,” International Medieval Congress, July 5-9</p>
 
-<p>2020, “Native English Influences on the Virgule in Middle English Verse,” Center for Medieval &amp; Renaissance Studies Annual Symposium, June 15-17, canceled due to pandemic.</p>
+<p>2020, “Native English Influences on the Virgule in Middle English Verse,” Center for Medieval &amp; Renaissance Studies Annual Symposium, June 15-17, canceled due to pandemic</p>
 
 <p>2019, “The Virgule in Context,” Mid-America Medieval Association Annual Symposium, September 12-13.</p>
 
-<p>2017, “An Exemplum ‘for the Wyves love of Bathe:’ Disrupting Form and Distributing Authority in the Clerk’s Tale,” Mid-America Medieval Association Annual Symposium, September 22.</p>
+<p>2017, “An Exemplum ‘for the Wyves love of Bathe:’ Disrupting Form and Distributing Authority in the Clerk’s Tale,” Mid-America Medieval Association Annual Symposium, September 22</p>
 
-<p>2017, “Deconstructing Shakespeare’s Griselda<em>,</em>” South-Central Renaissance Conference, April 20-22.</p>
+<p>2017, “Deconstructing Shakespeare’s Griselda<em>,</em>” South-Central Renaissance Conference, April 20-22</p>
 
-<p>2016, “The Merchant, a Marriage, and Received Authority: Posing an Ideological Challenge to the Medieval Social Order in the Merchant’s Tale<em>,</em>” Midwest Modern Language Association Conference, November 10-13.</p>
+<p>2016, “The Merchant, a Marriage, and Received Authority: Posing an Ideological Challenge to the Medieval Social Order in the Merchant’s Tale<em>,</em>” Midwest Modern Language Association Conference, November 10-13</p>
 
-<p>2015, “Inversions of Social Order in the Clerk’s Tale<em>:</em> The Clerk’s Case for Powerful Wives and Common People,” Missouri State University Undergraduate Literature Conference, April 17-18.</p>
+<p>2015, “Inversions of Social Order in the Clerk’s Tale<em>:</em> The Clerk’s Case for Powerful Wives and Common People,” Missouri State University Undergraduate Literature Conference, April 17-18</p>
 
 </td>
 </tr>
@@ -133,15 +135,24 @@ const Cv = () => {
 </tr>
 
 <tr>
-<td valign="top"><p>RESEARCH EXPERIENCE</p></td>
-<td valign="top"><p><strong>Fall 2018-Spring 2021, Research Assistant</strong></p>
+<td valign="top"><p>PROFESSIONAL EXPERIENCE</p></td>
+<td valign="top"><p><strong>Spring 2022 - Present, Project Manager</strong></p></td>
+<p>An Electronic Supplement to <em>The Text of the Canterbury Tales</em>, Self-Employed</p>
+<p>My project to digitize and transcribe John Manly and Edith Rickert's sixty thousand collation cards recording the textual history of the Canterbury Tales is completely funded by grants and scholarships. The result will be a web-based digital supplement that corrects data in Manly and Rickert's The Text of the Canterbury Tales and improves the accessibility of their text. I am collaborating with University of Chicago Special Collections and technical departments at the University of Oklahoma, including the Digitization Lab and the Digital Scholarship Lab, on digitization standards, segmentation, optical character recognition, and website development.</p>
+    
+    </tr>
+    <td>
+        <tr><p><strong>Summer 2021, Student Library Assistant</strong></p></tr>
+        <p>Digitization Lab, University of Oklahoma Libraries</p>
+        <p>To achieve archival-quality preservation of items in the University of Oklahoma libraries’ collections, I operated flat-bed, V-shaped, and large-format scanners (as well as a camera), processed the images taken by cropping, color correction, attributing metadata, or applying optical character recognition, and prepared them for use or storage. I adhered to a project workflow to track production deadlines and comply with industry standards in digitization.</p>
+   </td>
+   <tr> <p><strong>Fall 2018 - Spring 2021, Research Assistant</strong></p>
 
 <p>Variorum Chaucer, University of Oklahoma </p>
 
-<p>The Variorum Chaucer produces comprehensive editions of Chaucer's individual works, and my role in our forthcoming publication is to write the physical descriptions of twenty-three printed editions containing the Pardoner’s Tale.</p>
-
-</td>
+<p>The Variorum Chaucer produces comprehensive editions of Chaucer's individual works. My role was to conduct qualitative and quantitative research, summarize my findings in oral or written reports, and write or edit final content to be published in the forthcoming volume on the Pardoner’s Prologue and Tale. I managed projects according to a workflow that I created to ensure comprehensive and accurate results. </p>
 </tr>
+
 
 <tr>
 <td valign="top"><p>COMMUNITY OUTREACH</p></td>
